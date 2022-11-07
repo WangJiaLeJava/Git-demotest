f3e82639c98c0131a34f2dd82c4ade60b294673e
WangJiaLeJava <1491710707@qq.com>
2022-11-07 18:25:58 +0800

    demo test

--- src/JingQianChao.java
+++ src/JingQianChao.java
@@ -1,5 +1,7 @@
 public class JingQianChao {
     public static void main(String[] args) {
         System.out.println("hello 小曹");
+        System.out.println("hello 小曹");
+        System.out.println("hello 小曹");
     }
 }
